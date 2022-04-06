21b2233ac94fc240e49ac05104e1f248123c2225
Mgebro01 <iakobmgebrishvili@gmail.com>
2022-04-06 15:26:30 +0400

    homework 2

--- lessons/lessons.py
+++ lessons/lessons.py
@@ -1,18 +1,18 @@
 Number = int(input('შეიყვანე მებისმირეი რიცხვი 0-100 :'))
 
-if Number > 100 or Number < 0:
+if Number > 100 and Number < 0:
     print()
-elif Number > 0 or Number < 40:
+elif Number >= 0 and Number <= 40:
     print('Failed')
-elif Number > 41 or Number < 50:
+elif Number >= 41 and Number <= 50:
     print('Fx')
-elif Number > 51 or Number < 60:
+elif Number >= 51 and Number <= 60:
     print('E')
-elif Number > 61 or Number < 70:
+elif Number >= 61 and Number <= 70:
     print('D')
-elif Number > 71 or Number < 80:
+elif Number >=71 and Number <= 80:
     print('C')
-elif Number > 81 or Number < 90:
+elif Number >= 81 and Number <= 90:
     print('B')
-elif Number > 91 or Number < 100:
+elif Number >= 91 and Number <= 100:
     print('A')
