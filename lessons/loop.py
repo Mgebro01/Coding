7b0a942d4864857248130eed719667adb1766952
Mgebro01 <iakobmgebrishvili@gmail.com>
2022-04-14 22:31:23 +0400

    homework 5

--- lessons/loop.py
+++ lessons/loop.py
@@ -1,4 +1,4 @@
-number = 1
+# number = 1
 # while True:
 #     number += 1
 #     if number % 4 == 0:
@@ -24,9 +24,25 @@ number = 1
 # for i in range(0, 10, 2):
 #     print(i)
 
+# Number = int(input('input number:'))
+#
+# for i in range(Number, 0, -1):
+#
+#     if Number % i == 0:
+#         print(i)
+
+
 Number = int(input('input number:'))
 
-for i in range(Number, 0, -1):
+Number2 = int(input('input number:'))
+Low = Number2
 
-    if Number % i == 0:
+if Number < Number2:
+
+    Low = Number
+
+for i in range(1, Low + 1):
+
+    if Number % i == 0 and Number2 % i == 0:
         print(i)
+
