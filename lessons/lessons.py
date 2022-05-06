b35a9dcd2baf27875fe1d553ff8c055f5d29ffcc
Mgebro01 <iakobmgebrishvili@gmail.com>
2022-05-06 12:56:02 +0400

    homework 6

--- lessons/lessons.py
+++ lessons/lessons.py
@@ -31,3 +31,111 @@
 #     print(Year, 'არ არის, ნაკიანი წელი')
 # elif Year % 100 == 0 and Year % 4 == 0 and Year % 400 == 0:
 #     print(Year, 'არის, ნაკიანი წელი')
+
+#
+#
+# string = input('input string:')
+#
+# print(len(string))
+# #
+# #
+# #
+# string = input('input string:')
+#
+# string2 = input('input string:')
+#
+# string3 = string + string2
+#
+# print(string3)
+# #
+# #
+# string = input('input string:')
+#
+# count = 0
+#
+# for i in range(0, len(string)):
+#     if "a" == string[i]:
+#         count += 1
+#
+# print(count)
+#
+# #
+#
+#
+#
+string = "სწავლის ძირი მწარე არის, კენწეროში გატკბილდების"
+string2 = ''
+count = 0
+for i in range(0, 20):
+    if "ს" == string[i]:
+        count += 1
+    string2 += string[i]
+
+print(count)
+print(string2)
+#
+#
+#
+#
+text = 'Hello, this is example of string. Please, write this text and do some exercises'
+count = 0
+for i in range(len(text)):
+    if " " in text[i]:
+        count += 1
+count += 1
+print(count)
+#
+#
+#
+#
+#
+#
+text = 'Have a nice day.'
+i = len(text)
+
+while i > 0:
+    i -= 1
+    print(text[i])
+#
+#
+#
+#
+text = input('input string')
+count = 0
+for i in range(0, len(text)):
+    if text[i] in 'AaEeIiOoUu':
+        count += 1
+
+print(count)
+#
+#
+#
+#
+#
+#
+n = str(input('input number:'))
+print(len(n))
+#
+#
+#
+#
+#
+name = input('input name:')
+surename = input('input surename:')
+email = name + surename + '@gmail.com'
+print(email)
+#
+#
+#
+#
+#
+#
+#
+text = input('input text')
+
+
+
+
+
+
+
